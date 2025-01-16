2ebc13fe562125cb3b99d218ac83e5363f602706
AsyncX <1158271328@qq.com>
2025-01-16 15:29:05 -0500

    modify rss

--- src/pages/zh.xml.js
+++ src/pages/zh.xml.js
@@ -6,20 +6,24 @@ const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("posts");
+
+  // 按日期降序排序（最新的在前）
+  const sortedPosts = posts.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate));
+
   return rss({
     title: 'AsyncX | 嘿!我是AX!',
     description: 'AsyncX的博客-i18n多语言/内容聚合/编程/哲学/爱好',
     site: context.site,
-    items: posts.map((post) => ({
+    items: sortedPosts.map((post) => ({
         title: post.data.title,
         link: `/posts/${post.slug.split('/').pop()}`, // 修改链接部分以获取 "zh/XX" 中的 "XX"
         content: sanitizeHtml(parser.render(post.body), {
-            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']) //渲染组件
+            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']), //渲染组件
         }),
         ...post.data,
     })),
-    customData:`
+    customData: `
     <language>zh-CN</language>
     `
-});
+  });
 }
